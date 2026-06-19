997c03dc851c07eeb95f0c40f6db962435861520
marie kanno <marie.abe0306@gmail.com>
2026-06-19 13:51:04 +0900

    提出前最終調整add1

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -19,7 +19,7 @@ class ContactFactory extends Factory
     public function definition(): array
     {
         return [
-            'category_id' => Category::inRandomOrder()->first()->id,
+            'category_id' => Category::factory(),
             'first_name' => fake()->firstName(),
             'last_name' => fake()->lastName(),
             'gender' => fake()->numberBetween(1, 3),
